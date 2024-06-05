a7d1543a9546587bb06cede42f03577724f55007
yopi <mjmyopi@gmail.com>
2024-06-05 10:40:18 +0700

    menambhakn angka 3 pada halaman dashboard sales

--- app/dashboard.php
+++ app/dashboard.php
@@ -14,7 +14,7 @@
               <div class="card-header">
                 <h3 class="card-title">
                   <i class="fas fa-chart-pie mr-1"></i>
-                  Sales
+                  Sales 3
                 </h3>
                 <div class="card-tools">
                   <ul class="nav nav-pills ml-auto">
